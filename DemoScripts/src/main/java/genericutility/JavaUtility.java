7562591189bf507d1d450b491119cee120c3f860
rajak <rajak@RajaBalaji>
2024-03-22 17:30:36 +0530

    Random Number Commit

---
+++ DemoScripts/src/main/java/genericutility/JavaUtility.java
@@ -0,0 +1,14 @@
+package genericutility;
+
+import java.util.Random;
+
+public class JavaUtility {
+
+	public static void main(String[] args) {
+
+		Random r=new Random();
+		int random = r.nextInt(1000);
+	//	System.out.println(random);
+	}
+
+}
