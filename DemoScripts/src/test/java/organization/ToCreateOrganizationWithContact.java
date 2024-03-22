d58529f6e11029e63309f27e5a14556ec898b8d7
rajak <rajak@RajaBalaji>
2024-03-22 11:00:05 +0530

    Organization Commit

---
+++ DemoScripts/src/test/java/organization/ToCreateOrganizationWithContact.java
@@ -0,0 +1,10 @@
+package organization;
+
+public class ToCreateOrganizationWithContact {
+
+	public static void main(String[] args) {
+
+		System.out.println("Contact with organization is completed");
+	}
+
+}
